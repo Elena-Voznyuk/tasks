4e2ccfe8047e6d51ac8ccababeefb39f75635e30
Elena <voznyuk_elena@inbox.ru>
2023-04-24 18:28:23 +0300

    feat:perform second task

--- Program.cs
+++ Program.cs
@@ -10,3 +10,22 @@ else Console.WriteLine($"Max number is {number_b}. Min number is {number_a}");
 */
 
 
+/*Console.WriteLine("Input your first number");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your second number");
+int number_b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your third number");
+int number_c = Convert.ToInt32(Console.ReadLine());
+int max = number_a;
+if (number_b > number_a)
+{
+    max = number_b;
+}
+if (number_c > max)
+{
+    max = number_c;
+}
+
+Console.WriteLine($"Max number is {max}");
+*/
+
