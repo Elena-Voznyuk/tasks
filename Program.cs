1521b0a5cecde820600560d9cbfba123e71ad1ea
Elena <voznyuk_elena@inbox.ru>
2023-04-24 17:03:30 +0300

    feat: add comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Input your first number");
+﻿/*Console.WriteLine("Input your first number");
 int number_a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input your first number");
 int number_b = Convert.ToInt32(Console.ReadLine());
@@ -7,3 +7,6 @@ if (number_a > number_b)
     Console.WriteLine($"Max number is {number_a}. Min number is {number_b}");
 }
 else Console.WriteLine($"Max number is {number_b}. Min number is {number_a}");
+*/
+
+
