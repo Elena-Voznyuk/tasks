ea4af3c7a2635816858612febdaa8104267c8ec5
Elena <voznyuk_elena@inbox.ru>
2023-04-24 16:56:43 +0300

    feat: decide first task

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input your first number");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your first number");
+int number_b = Convert.ToInt32(Console.ReadLine());
+if (number_a > number_b)
+{
+    Console.WriteLine($"Max number is {number_a}. Min number is {number_b}");
+}
+else Console.WriteLine($"Max number is {number_b}. Min number is {number_a}");
